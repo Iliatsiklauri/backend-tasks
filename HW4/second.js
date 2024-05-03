0e9e8384921d1504e5754e573091f6d21bbb5f8e
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-05-04 01:39:51 +0400

    fix:removed unused data declaration

--- HW4/second.js
+++ HW4/second.js
@@ -9,7 +9,7 @@ program
   .argument('<city>', 'city you want to see weather of')
   .action(async (city) => {
     try {
-      const data = await axios
+      await axios
         .get(
           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
         )
