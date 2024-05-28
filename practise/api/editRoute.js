316e99aedcd7aa6091b9e296fa829a6b6a585d59
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-05-28 22:01:15 +0400

    commit

---
+++ practise/api/editRoute.js
@@ -0,0 +1,21 @@
+const e = require('express');
+const { Router } = require('express');
+const { readData } = require('../service/readData');
+const writeData = require('../service/writeData');
+const editRoute = Router();
+
+editRoute.put('/', async (req, res) => {
+  const response = await readData('data.json');
+  const id = Number(req.query.id);
+  const obj = response.find((el) => el.id === id);
+  obj.name = req.query.name;
+  obj.age = req.query.age;
+  await writeData(response);
+  if (!obj) {
+    res.send({ success: false });
+    return;
+  }
+  res.send({ succes: true });
+});
+
+module.exports = editRoute;
