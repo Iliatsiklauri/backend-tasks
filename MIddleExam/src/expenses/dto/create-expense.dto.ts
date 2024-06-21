b59754944bfa55205321f10f417cb1d487e53ed4
ilia tsiklauri <tsiklauri.ilia26@gmail.com>
2024-06-22 00:45:12 +0400

    commit

---
+++ MIddleExam/src/expenses/dto/create-expense.dto.ts
@@ -0,0 +1,12 @@
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateExpenseDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  category: string;
+
+  @IsNumber()
+  cost: number;
+}
